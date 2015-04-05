76d284e5fb698c475c0cd9579082ef66180423a0
yepyao <yepyao@gmail.com>
2015-04-05 22:48:14 +0800

    add result

---
+++ tmall2015/Tmall/src/main/Stat.java
@@ -0,0 +1,10 @@
+package main;
+
+import data.DataReader;
+
+public class Stat {
+	public static void main(String[] args) {
+		DataReader reader = new DataReader("data\\");
+		
+	}
+}
--- tmall2015/Tmall/src/model/RuleBased.java
+++ tmall2015/Tmall/src/model/RuleBased.java
@@ -16,7 +16,7 @@ public class RuleBased extends Model{
 		Result result = new Result();
 		for(UserBehavior bhv:trainData){
 			if ((bhv.behaviorType == BehaviorType.BUY || bhv.behaviorType == BehaviorType.CART) 
-					&& bhv.time.beforeTrain()<5*24*3600*1000 && TrainItemData.checkItem(bhv.itemId))
+					&& bhv.time.beforeTrain()<1*24*3600*1000 && TrainItemData.checkItem(bhv.itemId))
 				result.addResultItem(bhv.userId, bhv.itemId);
 		}
 		return result;
